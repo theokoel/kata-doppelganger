1976d455c762339728fc737a4b2553c5624cab40
Theo Koel <koeltheo@gmail.com>
2022-11-09 14:50:18 +0100

    Feat : Test calculator

--- java/src/test/java/fr/efrei/CalculatorTest.java
+++ java/src/test/java/fr/efrei/CalculatorTest.java
@@ -1,10 +1,24 @@
 package fr.efrei;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
+class CustomAuth implements Authorizer {
+
+  @Override
+  public boolean authorize() {
+    // TODO Auto-generated method stub
+    return true;
+  }
+
+}
 
 public class CalculatorTest {
   @Test
   void should_throw_when_not_authorized() {
     // TODO: trigger the bug in Calculator.divide() by implementing the Authorizer interface
+    Calculator calc = new Calculator(new CustomAuth());
+    assertEquals(3, calc.divide(6, 2));
   }
 }
